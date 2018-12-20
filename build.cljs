605f022ee2a6516e6b9375f44c98ef47d2f54e13
Pankaj Doharey <pankajdoharey@gmail.com>
2018-12-20 21:02:57 +0530

    Updates build.cljs for release and dev-build commands, updates package.json start and release script to use local installed muon.

--- build.cljs
+++ build.cljs
@@ -1,10 +1,34 @@
 (require '[lumo.build.api :as b])
 
-(b/build
- (b/inputs "src")
- {:main 'app.main.core
-  :output-to "app/main.js"
-  :output-dir "out/"
-  :optimizations :none
-  :target :nodejs
-  :infer-externs true})
+
+(defn dev-build []
+ (b/build
+  (b/inputs "src")
+  {:main 'app.main.core
+   :output-to "app/main.js"
+   :output-dir "out/"
+   :optimizations :none
+   :target :nodejs
+   :infer-externs true})
+  (.log js/console "Dev Build compiled.."))
+
+(defn release []
+  (b/build
+   (b/inputs "src")
+   {:main 'app.main.core
+    :output-to "app/main.js"
+    :output-dir "out/"
+    :optimizations :simple
+    :target :nodejs
+    :infer-externs true})
+  (.log js/console "Release Build compiled"))
+
+(defn -main []
+  (let [args js/process.argv
+        command (last args)]
+    (.log js/console "Command: " command)
+    (case command
+      "release" (release)
+      (dev-build))))
+
+(-main)
