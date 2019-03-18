396d30e61fec3eec91e8f20c609542f842d5f3bb
Alex Johnson <notalexjohnson@gmail.com>
2019-03-18 18:22:47 +0530

    Extract electronish functions to separate namespace

--- src/app/main/window.cljs
+++ src/app/main/window.cljs
@@ -1,10 +1,9 @@
 (ns app.main.window
-  (:require [electron :as electron]
+  (:require [app.main.electro :as electro]
             [app.main.cli :as cli]
             [app.main.config :as config]
             [app.main.keyboard :as kb]))
 
-(def main-window (atom nil))
 (def tabs (atom []))
 (def current-tab (atom nil))
 (def web-pref {:title "B42" :webPreferences {:nodeIntegration true}
@@ -16,26 +15,11 @@
 (defn set-current-tab [view]
   (reset! current-tab view))
 
-(defn set-browser-view [view]
-  (.setBrowserView @main-window view))
-
-(defn create-browser-view
-  ([pref url] (create-browser-view pref url 0 0))
-  ([pref url x y]
-   (let [view (new electron/BrowserView)
-         width (pref "width")
-         height (pref "height")]
-
-     (doto view
-       (set-browser-view)
-       (.setBounds #js{:x x :y y :width width :height (- height 42)})
-       (.webContents.loadURL url)))))
-
 (defn switch-view [view]
-  (set-browser-view view))
+  (electro/set-browser-view view))
 
 (defn create-tab [pref url]
-  (let [view (create-browser-view pref url)]
+  (let [view (electro/create-browser-view pref url)]
     (switch-view view)
     (add-tab view)
     (set-current-tab view)))
@@ -47,8 +31,8 @@
 (defn init-browser [size]
   (cli/start-msg)
   (let [web-pref (merge web-pref size)]
-    (reset! main-window (new electron/BrowserWindow (clj->js web-pref)))
-    (.loadURL @main-window "http://localhost:3742")
+    (electro/create-main-window web-pref)
+    (electro/load-url-in-window "http://localhost:3742")
 
     (create-tab web-pref "https://yahoo.com")
     (create-tab web-pref "https://github.com")
@@ -59,10 +43,10 @@
     ;;Reload config with shortcut
 
     (kb/register-key-bindings (config/get-key-bindings))
-    (.on @main-window "closed" #(reset! main-window nil))))
+    (electro/reset-main-window-on-close)))
 
 (defn start-default-window
-  ([] (let[screen (.getPrimaryDisplay electron/screen)
+  ([] (let[screen (electro/get-primary-display)
            size (js->clj screen.workAreaSize)]
         (start-default-window size)))
   ([size] (init-browser size)))
